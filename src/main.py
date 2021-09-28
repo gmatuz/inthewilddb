f34bd96ad01fa82d1f32ef51641c661bfa694d1f
gabor matuz <gabor.matuz@real-digital.de>
2021-09-28 13:49:09 +0200

    adding readme and making wrapper easier to follow
    Former-commit-id: f6213dbaed383e5bcfbaccfb947d54c6fb251111

--- src/main.py
+++ src/main.py
@@ -18,17 +18,6 @@ def exploitations(vulnerability_id: str, format_cli: bool = True):
         print(json.dumps(exploitations))
 
 
-def get_exploitations(vulnerability_id: str):
-    exploitations = []
-    for row in cur.execute('SELECT id, referenceURL as url, timeStamp FROM exploits WHERE type="exploitation" AND id=?', [vulnerability_id]):
-        exploitation = collections.OrderedDict()
-        exploitation['id'] = row[0]
-        exploitation['referenceURL'] = row[1]
-        exploitation['timeStamp'] = row[2]
-        exploitations.append(exploitation)
-    return exploitations
-
-
 @app.command()
 def exploits(vulnerability_id: str, format_cli: bool = True):
     exploits = get_exploits(vulnerability_id)
@@ -37,10 +26,15 @@ def exploits(vulnerability_id: str, format_cli: bool = True):
     else:
         print(json.dumps(exploits))
 
-
 def get_exploits(vulnerability_id: str):
+    return __get_exploit_reports(vulnerability_id, "exploit")
+
+def get_exploitations(vulnerability_id: str):
+    return __get_exploit_reports(vulnerability_id, "exploitation")
+
+def __get_exploit_reports(vulnerability_id: str, type: str):
     exploits = []
-    for row in cur.execute('SELECT id, referenceURL, timeStamp FROM exploits WHERE type="exploit" AND id=?', [vulnerability_id]):
+    for row in cur.execute('SELECT id, referenceURL, timeStamp FROM exploits WHERE type=? AND id=?', [type, vulnerability_id]):
         exploit = collections.OrderedDict()
         exploit['id'] = row[0]
         exploit['referenceURL'] = row[1]
@@ -51,18 +45,19 @@ def get_exploits(vulnerability_id: str):
 
 @app.command()
 def reports(vulnerability_id: str, format_cli: bool = True):
-    report = get_reports(vulnerability_id)
+    report = get_report(vulnerability_id)
     if(format_cli):
         format_full_report_table(report)
     else:
         print(json.dumps(report))
 
 
-def get_reports(vulnerability_id: str):
-    return {"background": get_vulnerability_details(vulnerability_id), "exploitation": get_exploitations(vulnerability_id), "exploit": get_exploits(vulnerability_id)}
+def get_report(vulnerability_id: str):
+    details = get_vulnerability_description(vulnerability_id)
+    return { "id": details["id"], "description": details["description"], "exploitations": get_exploitations(vulnerability_id), "exploits": get_exploits(vulnerability_id)}
 
 
-def get_vulnerability_details(vulnerability_id: str):
+def get_vulnerability_description(vulnerability_id: str):
     cur.execute('SELECT id, description FROM vulns WHERE id=?',
                 [vulnerability_id])
     vulnerability = cur.fetchone()
@@ -88,23 +83,23 @@ def format_report_table(reports, vulnerability_id):
 def format_full_report_table(output):
     formatted = []
     formatted.append(["Vulnerability", "report", "URL", "Report Time"])
-    for exploitation in output["exploitation"]:
+    for exploitation in output["exploitations"]:
         formatted.append(
             ["", "", exploitation["referenceURL"], exploitation["timeStamp"]])
-    if(len(output["exploitation"]) == 0):
+    if(len(output["exploitations"]) == 0):
         formatted.append(["", "", "No reports of exploitation inTheWild", ""])
-    for exploit in output["exploit"]:
+    for exploit in output["exploits"]:
         formatted.append(
             ["", "", exploit["referenceURL"], exploit["timeStamp"]])
-    if(len(output["exploit"]) == 0):
+    if(len(output["exploits"]) == 0):
         formatted.append(["", "", "No reports of exploits available", ""])
-    formatted[1][0] = output["background"]["id"]
-    formatted[2][0] = output["background"]["description"]
+    formatted[1][0] = output["id"]
+    formatted[2][0] = output["description"]
     formatted[1][1] = "exploitation"
-    if(len(output["exploitation"]) == 0):
-        formatted[len(output["exploitation"])+2][1] = "exploit"
+    if(len(output["exploitations"]) == 0):
+        formatted[len(output["exploitations"])+2][1] = "exploit"
     else:
-        formatted[len(output["exploitation"])+1][1] = "exploit"
+        formatted[len(output["exploitations"])+1][1] = "exploit"
     table = Texttable(max_width=150)
     table.add_rows(formatted)
     print(table.draw())
